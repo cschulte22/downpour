2e7230ba2ec9dacea9877143cb7fb9341a802fdf
Chris Schulte <chris@oceanbreezesoftware.com>
2012-07-06 10:34:13 -0700

    Got it working with multiple matching items

--- jquery.downpour.js
+++ jquery.downpour.js
@@ -11,187 +11,203 @@
 
   var methods = {
     init : function( settings ) { 
-      console.log('downpour init');
-      console.log('downpour init called for ' + this.attr('id'));
+      return this.each(function() {
+        var $this = $(this);
 
-      settings = $.extend({}, defaults, settings);
+        console.log('downpour init');
+        console.log('downpour init called for ' + $this.attr('id'));
 
-      var id = this.attr('id');
+        settings = $.extend({}, defaults, settings);
 
-      var data = this.data('downpour') || {}
+        var id = $this.attr('id');
 
-      if (data.id) {
-        console.log('already setup, returning')
-        // Already setup
-        return this;
-      }
+        var data = $this.data('downpour') || {}
 
-      // Add a div with relative positioning around the select box
-      this.wrap('<div class="downpour_wrap ' + settings['class'] + '"/>');
-      var wrapper = this.parent('div.downpour_wrap');
-      wrapper.css({
-        'position': 'relative',
-        'overflow': 'visible'
-      });
-
-      // Hide the original select box
-      this.hide();
-
-      // Add our customized div select 'box' to the wrapper
-      wrapper.append('<div class="downpour_select_box" />');
-      _select_box(this).css({
-        position: 'absolute',
-        top: '0px',
-        left: '0px',
-        width: this.css('width')
-      });
-
-      _select_box(this).bind('click.downpour', function(event) {
-        event.stopPropagation(); 
-        _handle_select_click($(this).siblings('select')); 
-      });
-
-      wrapper.append('<div class="downpour_option_box" />');
-      _option_box(this).css({
-        display: 'none',
-        position: 'absolute',
-        top: '0px',
-        left: '0px',
-        'min-width': _select_box(this).outerWidth() + 'px'
-      });
-      var option_box = _option_box(this);
-      var selected_row = null;
-
-      this.find('option').each(function() {
-        var row = $('<div class="downpour_row">' + $(this).html() + '</div>');
-        row.data('downpour_value', $(this).attr('value'));
-        if (settings.nowrap) {
-          row.css({'white-space': 'nowrap'});
-        }
-        if ($(this).is(':selected')) {
-          selected_row = row;
+        if (data.id) {
+          console.log('already setup, returning')
+          // Already setup
+          return $this;
         }
 
-        option_box.append(row);
+        // Add a div with relative positioning around the select box
+        $this.wrap('<div class="downpour_wrap ' + settings['class'] + '"/>');
+        var wrapper = $this.parent('div.downpour_wrap');
+        wrapper.css({
+          'position': 'relative',
+          'overflow': 'visible'
+        });
+
+        // Hide the original select box
+        $this.hide();
+
+        // Add our customized div select 'box' to the wrapper
+        wrapper.append('<div class="downpour_select_box" />');
+        _select_box($this).css({
+          position: 'absolute',
+          top: '0px',
+          left: '0px',
+          width: $this.css('width')
+        });
+
+        _select_box($this).bind('click.downpour', function(event) {
+          event.stopPropagation(); 
+          _handle_select_click($(this).siblings('select')); 
+        });
+
+        wrapper.append('<div class="downpour_option_box" />');
+        _option_box($this).css({
+          display: 'none',
+          position: 'absolute',
+          top: '0px',
+          left: '0px',
+          'min-width': _select_box($this).outerWidth() + 'px'
+        });
+        var option_box = _option_box($this);
+        var selected_row = null;
+
+        $this.find('option').each(function() {
+          var row = $('<div class="downpour_row">' + $(this).html() + '</div>');
+          row.data('downpour_value', $(this).attr('value'));
+          if (settings.nowrap) {
+            row.css({'white-space': 'nowrap'});
+          }
+          if ($(this).is(':selected')) {
+            selected_row = row;
+          }
+
+          option_box.append(row);
+        });
+
+        console.log('selected row:')
+        console.log(selected_row);
+
+
+        // Grab the selected option from the select box (if any) and append the text to our custom select box
+        if (selected_row != null) {
+          _select_box($this).html(selected_row.html());
+        }
+        else {
+          _select_box($this).html('');
+        }
+
+        // Set the dimensions of the wrapper to the size of the select box so it flows correctly
+        wrapper.css({
+          height: _select_box($this).outerHeight(),
+          width: _select_box($this).outerWidth()
+        })
+
+        data.id = id;
+        data.settings = settings;
+
+        if (selected_row != null) {
+          data.selected = selected_row.data('downpour_value');
+        }
+
+        $this.data('downpour', data);
+
+        return $this;
       });
-
-      console.log('selected row:')
-      console.log(selected_row);
-
-
-      // Grab the selected option from the select box (if any) and append the text to our custom select box
-      if (selected_row != null) {
-        _select_box(this).html(selected_row.html());
-      }
-      else {
-        _select_box(this).html('');
-      }
-
-      // Set the dimensions of the wrapper to the size of the select box so it flows correctly
-      wrapper.css({
-        height: _select_box(this).outerHeight(),
-        width: _select_box(this).outerWidth()
-      })
-
-      data.id = id;
-      data.settings = settings;
-
-      if (selected_row != null) {
-        data.selected = selected_row.data('downpour_value');
-      }
-
-      this.data('downpour', data);
-
-      return this;
     },
 
     focus : function() {
-      console.log('Focus method called');
-      var select_box = _select_box(this);
-      var option_box = _option_box(this);
+      return this.each(function() {
+        var $this = $(this);
+        console.log('Focus method called');
+        var select_box = _select_box($this);
+        var option_box = _option_box($this);
 
-      if (!option_box.is(':visible')) {
-        option_box.css({
-          top: (select_box.outerHeight()) + 'px'
-        })
+        if (!option_box.is(':visible')) {
+          option_box.css({
+            top: (select_box.outerHeight()) + 'px'
+          })
 
-        option_box.find('div.downpour_row_hover').removeClass('downpour_row_hover');
-        option_box.find('div.downpour_row_selected').removeClass('downpour_row_selected');
+          option_box.find('div.downpour_row_hover').removeClass('downpour_row_hover');
+          option_box.find('div.downpour_row_selected').removeClass('downpour_row_selected');
 
-        var downpour_rows = option_box.find('div.downpour_row');
+          var downpour_rows = option_box.find('div.downpour_row');
 
-        downpour_rows.bind('mouseover.downpour', function() {
-          $(this).siblings('div.downpour_row_hover').removeClass('downpour_row_hover');
-          $(this).addClass('downpour_row_hover');
-        });
-        downpour_rows.bind('mouseout.downpour', function() {
-          $(this).removeClass('downpour_row_hover');
-        });
-        downpour_rows.bind('click.downpour', function() {
-          _grab_active_select().downpour('select');
-        });
-
-        $(document).bind('keydown.downpour', _handle_keypress);
-        $(document).bind('click.downpour', function() { _grab_active_select().downpour('blur')});
-
-        var data = this.data('downpour');
-        if (data.selected !== undefined && data.selected != null) {
-          option_box.find('div.downpour_row').each(function() {
-            if ($(this).data('downpour_value') == data.selected) {
-              $(this).addClass('downpour_row_hover downpour_row_selected');
-            }
+          downpour_rows.bind('mouseover.downpour', function() {
+            $(this).siblings('div.downpour_row_hover').removeClass('downpour_row_hover');
+            $(this).addClass('downpour_row_hover');
           });
-        }
+          downpour_rows.bind('mouseout.downpour', function() {
+            $(this).removeClass('downpour_row_hover');
+          });
+          downpour_rows.bind('click.downpour', function() {
+            _grab_active_select().downpour('select');
+          });
 
-        option_box.show();
-      }
+          $(document).bind('keydown.downpour', _handle_keypress);
+          $(document).bind('click.downpour', function() { _grab_active_select().downpour('blur')});
+
+          var data = $this.data('downpour');
+          if (data.selected !== undefined && data.selected != null) {
+            option_box.find('div.downpour_row').each(function() {
+              if ($(this).data('downpour_value') == data.selected) {
+                $(this).addClass('downpour_row_hover downpour_row_selected');
+              }
+            });
+          }
+
+          option_box.show();
+        }
+      });
     },
 
     blur: function() {
-      _hide_options(this);
+      return this.each(function() {
+        var $this = $(this);
+        _hide_options($this);
 
-      var data = this.data('downpour');
-      if (data.settings.blur !== undefined) {
-        console.log('blur event defined');
-        data.settings.blur.apply(this);
-      }
-      else {
-        console.log('blur event undefined');
-      }
+        var data = $this.data('downpour');
+        if (data.settings.blur !== undefined) {
+          console.log('blur event defined');
+          data.settings.blur.apply($this);
+        }
+        else {
+          console.log('blur event undefined');
+        }
+      });
     },
 
     select: function() {
-      var options = _option_box(this);
-      var current_row = options.find('div.downpour_row_hover');
+      return this.each(function() {
+        var $this = $(this);
+        var options = _option_box($this);
+        var current_row = options.find('div.downpour_row_hover');
 
-      this.downpour('selected', current_row.data('downpour_value'));
+        $this.downpour('selected', current_row.data('downpour_value'));
 
-      this.siblings('div.downpour_select_box').html(current_row.html());
+        $this.siblings('div.downpour_select_box').html(current_row.html());
 
-      _hide_options(this);
+        _hide_options($this);
 
-      var data = this.data('downpour');
-      if (data.settings.select !== undefined) {
-        console.log('select event defined');
-        data.settings.select.apply(this, [current_row.data('downpour_value'), current_row.html()]);
-      }
-      else {
-        console.log('select event undefined');
-      }
+        var data = $this.data('downpour');
+        if (data.settings.select !== undefined) {
+          console.log('select event defined');
+          data.settings.select.apply($this, [current_row.data('downpour_value'), current_row.html()]);
+        }
+        else {
+          console.log('select event undefined');
+        }
+      });
     },
 
     selected: function(value) {
-      var data = this.data('downpour');
-      if (value === undefined) {
-        // Return the existing value
-        return data.selected;
-      }
-      else {
-        // Set the currently selected value
-        data.selected = value;
-        // Set it on the underlying select box too
-        this.val(value);
-      }
+      return this.each(function() {
+        var $this = $(this);
+        var data = $this.data('downpour');
+        if (value === undefined) {
+          // Return the existing value
+          return data.selected;
+        }
+        else {
+          // Set the currently selected value
+          data.selected = value;
+          // Set it on the underlying select box too
+          $this.val(value);
+        }
+      });
     }
   };
 
